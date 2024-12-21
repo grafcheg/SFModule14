70b64a4a23154049afd9186751a21f6f0c19a916
Vitaly Efremov <dergraff@gmail.com>
2024-12-22 08:01:28 +1000

    Модуль 14

---
+++ SFModule14/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace SFModule14;
+
+public class Contact
+{
+    public Contact(string name, string lastName, long phoneNumber, string email)
+    {
+        Name = name;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    public String Name {get;}
+    public String LastName {get;}
+    public long PhoneNumber {get;}
+    public String Email {get;}
+}
